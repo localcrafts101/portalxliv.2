de2ab392ec51d3f77f5dc8a0e063bf82ab7de208
Rindo Child <53924846+rindo404@users.noreply.github.com>
2019-12-29 19:28:31 -0600

    flyover.BOSTON
    t

--- p5/warholSelfies/sketch.js
+++ p5/warholSelfies/sketch.js
@@ -1,3 +1,5 @@
+var array_chicago = [rtmp://live-ord02.twitch.tv/app/{stream_key},rtmp://live-ord03.twitch.tv/app/{stream_key}
+]
 function setup() {
   // put setup code here
   createCanvas(1920, 1080);
@@ -8,3 +10,4 @@ function draw() {
 
   ellipse(mouseX, mouseY, 5, 5) ;
 }
+createAr
